1505491f6069ae7c35d411d96cc878e83dc72d08
Santiago <saantii.28@hotmail.com>
2014-10-29 22:15:43 -0300

    Agrego anotaciones a Empleado Solamente a id y nombre

--- src/main/java/model/Empleado.java
+++ src/main/java/model/Empleado.java
@@ -1,12 +1,85 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Empleados")
 public class Empleado {
 
-	private int codigo;
+	@Id
+	@GeneratedValue
+	private int id;
+	
 	private String nombre;
+	
+	@Transient
 	private String apellido;
+	
+	@Transient
 	private String puesto;
+	
+	@Transient
 	private String telefono;
+	
+	@Transient
 	private String direccion;
+	
+	@Transient
 	private int salario;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getSalario() {
+		return salario;
+	}
+
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
 }

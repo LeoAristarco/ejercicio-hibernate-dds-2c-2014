2f9f260d7de43b174d51a773777bf4a7e79436ed
Santiago <saantii.28@hotmail.com>
2014-10-29 22:09:24 -0300

    Agrego script sql con tabla Proyectos

---
+++ sql/script.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA IF NOT EXISTS `prueba` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `prueba` ;
+
+CREATE  TABLE IF NOT EXISTS `prueba`.`Proyectos` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(50),
+
+	PRIMARY KEY (`id`)
+)

08ebca227ffb2304a326856eda7f7999e2662b3c
Santiago <saantii.28@hotmail.com>
2014-10-29 22:10:40 -0300

    Agrego un test de persistencia de un proyecto

--- src/test/java/db/ContextTest.java
+++ src/test/java/db/ContextTest.java
@@ -1,6 +1,11 @@
 package db;
 
+import model.Proyecto;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class ContextTest {
 
@@ -11,6 +16,24 @@ public class ContextTest {
 
 	@Test
 	public void contextUpWithTransaction() throws Exception {
-		EntityManagerHelper.withTransaction(() -> {});
+		EntityManagerHelper.withTransaction(() -> {
+		});
+	}
+
+	@Before
+	public void begin() throws Exception {
+		EntityManagerHelper.beginTransaction();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		EntityManagerHelper.commit();
+	}
+
+	@Test
+	public void agregoUnProyecto() {
+		Proyecto proyecto = new Proyecto();
+		proyecto.setNombre("Mi Proyecto");
+		EntityManagerHelper.persist(proyecto);
 	}
 }

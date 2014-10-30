696be15c323528fb0468b5f1182f65889a16f522
Santiago <saantii.28@hotmail.com>
2014-10-29 21:23:04 -0300

    Primer commit con clase Proyecto y Empleado

---
+++ src/main/java/model/Empleado.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Empleado {
+
+	private int codigo;
+	private String nombre;
+	private String apellido;
+	private String puesto;
+	private String telefono;
+	private String direccion;
+	private int salario;
+}

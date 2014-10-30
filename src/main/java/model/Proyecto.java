d256791ce274fd44d26f758313f9b754309efafd
Santiago <saantii.28@hotmail.com>
2014-10-29 22:10:25 -0300

    Agrego anotaciones a clase Proyecto

--- src/main/java/model/Proyecto.java
+++ src/main/java/model/Proyecto.java
@@ -1,7 +1,30 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Proyectos")
 public class Proyecto {
 
-	private int codigo;
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
 	private String nombre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }

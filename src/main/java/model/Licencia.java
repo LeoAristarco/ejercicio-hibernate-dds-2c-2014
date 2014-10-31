727e43ec249b27f3b471285f520f973764d03aa0
Santiago <saantii.28@hotmail.com>
2014-10-31 16:06:16 -0300

    Le agrego a Licencia los campos fecha inicio y vencimiento Ambos dos son de tipo Date

--- src/main/java/model/Licencia.java
+++ src/main/java/model/Licencia.java
@@ -1,14 +1,36 @@
 package model;
 
+import java.util.Date;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="Licencias")
 public class Licencia extends EntidadPersistente {
+	
+	private Date fecha_inicio;
+	
+	private Date fecha_vencimiento;
 
 	@OneToOne
 	@JoinColumn(name="razon_id")
 	private Razon razon;
+
+	public Date getFecha_inicio() {
+		return fecha_inicio;
+	}
+
+	public void setFecha_inicio(Date fecha_inicio) {
+		this.fecha_inicio = fecha_inicio;
+	}
+
+	public Date getFecha_vencimiento() {
+		return fecha_vencimiento;
+	}
+
+	public void setFecha_vencimiento(Date fecha_vencimiento) {
+		this.fecha_vencimiento = fecha_vencimiento;
+	}
 
 	public Razon getRazon() {
 		return razon;

37a70492b83972b8cac51cc4f37ae85594ab4183
Santiago <saantii.28@hotmail.com>
2014-10-31 15:32:30 -0300

    Agrego la clase Domicilio

---
+++ src/main/java/model/Domicilio.java
@@ -0,0 +1,32 @@
+package model;
+
+import javax.persistence.*;
+
+@Embeddable
+public class Domicilio {
+
+	private String direccion;
+	
+	private String ciudad;
+	
+	private String provincia;
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+}

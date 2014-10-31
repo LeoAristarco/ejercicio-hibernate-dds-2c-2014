7159ccc99e8f8b0e507239fab251b1e08b81de5b
Santiago <saantii.28@hotmail.com>
2014-10-31 15:53:00 -0300

    Agrego la clase Puesto

---
+++ src/main/java/model/Puesto.java
@@ -0,0 +1,17 @@
+package model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Puesto extends EntidadPersistente {
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}

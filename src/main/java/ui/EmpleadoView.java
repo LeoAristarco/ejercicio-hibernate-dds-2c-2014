e6f820a6fdd0cd60146719c80c4331676d0e2b3b
Santiago <saantii.28@hotmail.com>
2014-11-04 14:56:15 -0300

    Agrego clase EmpleadoView que hereda de SimpleWindow

---
+++ src/main/java/ui/EmpleadoView.java
@@ -0,0 +1,26 @@
+package ui;
+
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.WindowOwner;
+
+@SuppressWarnings("serial")
+public class EmpleadoView extends SimpleWindow<EmpleadoViewModel> {
+
+	public EmpleadoView(WindowOwner parent) {
+		super(parent, new EmpleadoViewModel());
+	}
+
+	@Override
+	protected void addActions(Panel actionsPanel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createFormPanel(Panel mainPanel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
